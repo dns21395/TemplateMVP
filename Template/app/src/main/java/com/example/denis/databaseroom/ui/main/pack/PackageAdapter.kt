2581aeb918714b707712c81db38298df750575e4
dns21395 <dns21395@gmail.com>
2017-12-14 19:18:33 +0900

    image loading became better

--- Template/app/src/main/java/com/example/denis/databaseroom/ui/main/pack/PackageAdapter.kt
+++ Template/app/src/main/java/com/example/denis/databaseroom/ui/main/pack/PackageAdapter.kt
@@ -2,16 +2,23 @@ package com.example.denis.databaseroom.ui.main.pack
 
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.drawable.BitmapDrawable
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Adapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.module.AppGlideModule
 import com.example.denis.databaseroom.R
 import com.example.denis.databaseroom.data.db.model.MyDatabase
+import com.example.denis.databaseroom.utils.GlideApp
 import com.example.denis.databaseroom.utils.getApplicationName
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.item_package.view.*
+
 
 /**
  * Created by denis on 14/12/2017.
@@ -19,9 +26,7 @@ import kotlinx.android.synthetic.main.item_package.view.*
 class PackageAdapter(var context: Context): RecyclerView.Adapter<PackageAdapter.PackageViewHolder>() {
     private var items = ArrayList<MyDatabase>()
     var recyclerView: RecyclerView? = null
-
-
-
+    
     override fun onBindViewHolder(holder: PackageViewHolder, position: Int) {
         holder.onBind(position)
     }
@@ -51,19 +56,26 @@ class PackageAdapter(var context: Context): RecyclerView.Adapter<PackageAdapter.
 
     inner class PackageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun onBind(position: Int) = with(itemView) {
-            val pack = items[position]
-
-            val iconDrawable = context.packageManager.getApplicationIcon(pack.pack)
-            val applicationName = getApplicationName(context, pack.pack)
-
-            text.text = applicationName
-
-            Glide
-                    .with(itemView)
-                    .load(iconDrawable)
-                    .into(icon)
-
-            count.text = "${pack.length}"
+            Observable.fromCallable {
+                try {
+                    val pack = items[position]
+                    context.packageManager.getApplicationIcon(pack.pack) as BitmapDrawable
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }.subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe {
+                        val pack = items[position]
+                        GlideApp
+                                .with(itemView.context)
+                                .load(it)
+                                .placeholder(R.mipmap.ic_launcher)
+                                .into(icon)
+                        val applicationName = getApplicationName(context, pack.pack)
+                        text.text = applicationName
+                        count.text = "${pack.length}"
+                    }
         }
 
     }
---
+++ Template/app/src/main/java/com/example/denis/databaseroom/utils/GlideModule.kt
@@ -0,0 +1,11 @@
+package com.example.denis.databaseroom.utils
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+/**
+ * Created by denis on 14/12/2017.
+ */
+@GlideModule
+class GlideModule : AppGlideModule() {
+}
